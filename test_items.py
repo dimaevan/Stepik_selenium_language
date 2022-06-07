16bdac8ac055413096bf53512c5b9b4b386f87a1
dimaevan <dimaevan@gmail.com>
2022-06-07 21:19:02 +0300

    Final

--- test_items.py
+++ test_items.py
@@ -1,9 +1,12 @@
 import time
+from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language(browser):
+def test_add_to_basket_button(browser):
     browser.get(link)
     print("Timer for check for browser language...")
-    time.sleep(20)
+    time.sleep(30)
+    add_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    assert add_button, "Add to basket button not founded"
